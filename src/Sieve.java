243a5948c85fe9bb1580508142f474e857af79cd
Alex Chiang <alex.chiang919@student.mckinneyisd.net>
2017-02-17 12:16:03 -0600

    Updated Sieve - Added Code
    Sieve before wasn't coded at all, was just the template itself. Now
    Sieve has code with correct functionality.

--- src/Sieve.java
+++ src/Sieve.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Sieve {
 
-	private static final String PROBLEM = "";
+	private static final String PROBLEM = "sieve";
 	private static final String EXT = ".dat";
 
 	public static void main(String[] args) {
@@ -15,7 +15,93 @@ public class Sieve {
 			printF(true, "File not found: %s", ex.getMessage());
 			return;
 		}
+		while (scan.hasNextLine()) {
+			String[] split = scan.nextLine().split(" ");
+			int a = Integer.parseInt(split[0]);
+			int b = Integer.parseInt(split[1]);
+			int[] array = new int[b - a + 1];
+			fillArray(array, a, b);
+			int sieve = 2;
+			printArray(array);
+			while (!allPrimes(array)) {
+				if (testSieve(array, sieve)) {
+					printLine("sieve = " + sieve);
+				} else {
+					sieve++;
+					continue;
+				}
+				sieve(array, sieve++);
+				printArray(array);
+			}
+			printLine();
+		}
 		scan.close();
+	}
+	
+	private static void fillArray(int[] array, int a, int b) {
+		int index = 0;
+		for (int i = a; i <= b; i++) {
+			array[index++] = i;
+		}
+	}
+	
+	private static boolean testSieve(int[] array, int n) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] <= n)
+				continue;
+			if (array[i] % n == 0)
+				return true;
+		}
+		return false;
+	}
+	
+	private static void sieve(int[] array, int n) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] <= n)
+				continue;
+			if (array[i] % n == 0)
+				array[i] = 0;
+		}
+	}
+	
+	private static boolean allPrimes(int[] array) {
+		for (int i : array) {
+			if (i == 0)
+				continue;
+			if (!prime(i))
+				return false;
+		}
+		return true;
+	}
+	
+	private static boolean prime(int number) {
+		if (number <= 2)
+			return true;
+		for (int i = 2; i < number; i++) {
+			if (number % i == 0)
+				return false;
+		}
+		return true;
+	}
+	
+	private static void printArray(int[] array) {
+		for (int i : array) {
+			if (i == 0) {
+				print("*");
+			} else {
+				print(i);
+			}
+			print(constructSpaces(4 - String.valueOf(i).length()));
+		}
+		printLine();
+	}
+	
+	private static String constructSpaces(int spaces) {
+		String s = "";
+		for (int a = 0; a < spaces; a++) {
+			s += " ";
+		}
+		return s;
 	}
 
 	public static void print(Object... o) {
